f29542411332cb34a95ff260ef6abca23401ac1f
onikun94 <onikun94@gmail.com>
2022-05-29 03:43:01 +0900

    change error message

--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,13 @@ from .serializers import BlogSerializer,TitleSerializer
 
 from django.views.decorators.csrf import requires_csrf_token
 from django.http import HttpResponseServerError
+
 @requires_csrf_token
 def my_customized_server_error(request, template_name='500.html'):
     import sys
     from django.views import debug
     error_html = debug.technical_500_response(request, *sys.exc_info()).content
-    return HttpResponseServerError(error_html)
+    return HttpResponseServerError('<h1>意味わからないエラー</h1>')
 
 
 class ListBlog(generics.ListCreateAPIView):
