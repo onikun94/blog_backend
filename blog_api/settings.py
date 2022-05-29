98faf8495f9be5b70c8df0b64aaa6ce9875d4a8a
onikun94 <onikun94@gmail.com>
2022-05-29 10:11:23 +0900

    change media url

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/images/'
+MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 
