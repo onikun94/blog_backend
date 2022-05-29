91aa1e6cf9b69ec2c96ceb0453f90ef79f9779bd
onikun94 <onikun94@gmail.com>
2022-05-29 11:17:37 +0900

    onlyblog add image

--- blog/views.py
+++ blog/views.py
@@ -6,15 +6,6 @@ from .models import Blog
 from .serializers import BlogSerializer,TitleSerializer
 
 
-
-
-class ListBlog(generics.ListCreateAPIView):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
-    def get_queryset(self):
-        queryset = Blog.objects.all()
-        return queryset
-
 class OnlyBlog(generics.ListCreateAPIView):
     serializer_class = BlogSerializer
     def get(self,request,pk):
@@ -29,36 +20,15 @@ class OnlyBlog(generics.ListCreateAPIView):
                 'created_at':blog.created_at,
                 'updated_at':blog.updated_at,
                 'title':blog.title,
-                'contents':blog.contents
+                'contents':blog.contents,
+                'image':blog.image
             }
             return Response(res)
         except:
-            return Response("i don't know server error",status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-# class ListTitle(generics.ListCreateAPIView):
-#     queryset = Blog.objects.filter('title')
-#     print("--------------")
-#     print(queryset)
-#     print("--------------")
-#     serializer_class = TitleSerializer
-#     def get_queryset(self):
-#         queryset = Blog.objects.all()
-#         print("+++++++++++++")
-#         print(queryset)
-#         print("+++++++++++++")
-#         return queryset
-# #初期ページでこっち
-class ListTitle(APIView):
-    def get(self,request):
-        blog = Blog.objects.order_by('-created_at')
-        res = [
-            {'id':b.id,'updated_at':b.updated_at,'title':b.title}
-        
-        for b in blog
-        ]
-        return Response(res)
 
-class ListTest(generics.ListCreateAPIView):
+class ListAll(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
     
